81432bb88bc4290b65c951530c37c53f80aa2851
grovem74 <matthewg74@gmail.com>
2020-06-01 16:00:22 -0600

    made adjustments

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,44 +1,47 @@
 const router = require("express").Router();
 const db = require("../models/workout.js");
 
+
+
+
 router.get("/api/workouts", (req, res) => {
-  db.Workout.find({})
-      .then(dbWorkout => {
-          res.json(dbWorkout);
-      })
-      .catch(err => {
-          res.json(err);
-      });
+    db.Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
 });
 
 router.post("/api/workouts", ({ body }, res) => {
-  db.Workout.create(body)
-      .then(dbWorkout => {
-          res.json(dbWorkout);
-      })
-      .catch(err => {
-          res.json(err);
-      });
+    db.Workout.create(body)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
 });
 
 router.put("/api/workouts/:id", (req, res) => {
-  db.Workout.findOneAndUpdate({ _id: req.params.id }, { $push: { "exercises": req.body } }, { new: true })
-      .then(dbWorkout => {
-          res.json(dbWorkout);
-      })
-      .catch(err => {
-          res.json(err);
-      });
+    db.Workout.findOneAndUpdate({ _id: req.params.id }, { $push: { "exercises": req.body } }, { new: true })
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
 });
 
 router.get("/api/workouts/range", (req, res) => {
-  db.Workout.find({}, (error, range) => {
-      if (error) {
-          console.log(error);
-      } else {
-          res.json(range);
-      }
-  });
+    db.Workout.find({}, (error, range) => {
+        if (error) {
+            console.log(error);
+        } else {
+            res.json(range);
+        }
+    });
 });
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,26 +1,30 @@
-// // dependencies
 const express = require("express");
-const PORT = process.env.PORT || 8080;
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const path = require ("path");
+
+//const Workout = require("./models/workoutModel");
+
+const PORT = process.env.PORT || 3000;
+
 const app = express();
-const mongoose = require('mongoose');
-const path = require("path");
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(express.static("public"));
 
-app.use(express.urlencoded({ extended: false }));
-app.set('view engine', 'html');
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user1:password1@ds263107.mlab.com:63107/heroku_9b74f6p7", { useNewUrlParser: true });
+
+app.use(require ("./routes/htmlRoutes"))
+app.use(require ("./routes/apiRoutes"))
 
 
-// routes
-require("./routes/api-routes")(app);
-require("./routes/html-routes")(app);
 
-
-// Connect to database
-mongoose.connect(process.env.MONGODB_URI || "mongodb://user:workout1@ds161144.mlab.com:61144/heroku_b3h1pvl1", { useNewUrlParser: true }, () => {
-    console.log('connected to mongodb');
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
 });
 
-app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
-});
+
